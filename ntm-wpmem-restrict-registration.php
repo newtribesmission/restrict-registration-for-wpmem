51d3dd90f80cf14953549488466cabf2c23b0e24
stevish-com <stevish@gmail.com>
2014-04-24 10:24:11 -0500

    fix license info ntmtc-wpmem-restrict-regi...php
    Add and Correct Copyright, License and Owner information

--- ntm-wpmem-restrict-registration.php
+++ ntm-wpmem-restrict-registration.php
@@ -2,9 +2,24 @@
 /*
 Plugin Name: NTM Restricted Registration
 Description: Restricts registration to email addresses listed within this file (edit the file to add/remove/edit email addresses or domains). Includes both whitelist (accepted emails) and blacklist (blocked emails). The blacklist will override entries in the whitelist
-Author: Stephen Narwold
-Author URI: http://blogs.ntm.org/stephen-narwold
+Author: New Tribes Mission (Stephen Narwold)
 Version: 1.2
+
+    Copyright (C) 2014 New Tribes Mission
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /*************************************/
