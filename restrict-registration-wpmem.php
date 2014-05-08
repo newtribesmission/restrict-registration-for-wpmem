9668da1cd4c564cd459f9a7f21958a38bd999491
Stephen Narwold <stevish@gmail.com>
2014-05-07 23:10:53 -0500

    Add WP Options Menu code: untested

--- restrict-registration-wpmem.php
+++ restrict-registration-wpmem.php
@@ -4,7 +4,7 @@ Plugin Name: Restrict Registration By Email for WP-Members
 Description: Restricts registration to email addresses listed within the options file (edit the options.php file to add/remove/edit email addresses or domains). Includes both whitelist (accepted emails) and blacklist (blocked emails). The blacklist will override entries in the whitelist
 Author: New Tribes Mission (Stephen Narwold)
 Plugin URI: http://wordpress.org/plugins/restrict-registration-for-wp-members/
-Version: 1.4.1
+Version: 1.5
 
     Copyright (C) 2014 New Tribes Mission
 
@@ -171,5 +171,97 @@ if ( file_exists(plugin_dir_path(__FILE__) . 'options.php') ) {
 	}
 	add_action( 'admin_notices', 'ntmrr_admin_notice' );
 }
+
+
+/******************/
+/** Options Page **/
+/******************/
+function ntmrr_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	$reg_form_msg = get_option('ntmrr_registration_form_message');
+	$error_msg = get_option('ntmrr_email_not_approved_message');
+	$redirect = get_option('ntmrr_redirect_on_unapproved');
+	$url = get_option('ntmrr_redirect_on_unapproved_url');
+	$whitelist = get_option('ntmrr_whitelisted_emails');
+	$blacklist = get_option('ntmrr_blacklisted_emails');
+	
+	//Update options on form submission
+    if( isset($_POST['ntmrr_posted']]) && $_POST['ntmrr_posted'] == 'Y' ) {
+        update_option( 'ntmrr_registration_form_message', $_POST['ntmrr_registration_form_message'] );
+		update_option( 'ntmrr_email_not_approved_message', $_POST['ntmrr_email_not_approved_message'] );
+		update_option( 'ntmrr_redirect_on_unapproved', $_POST['ntmrr_redirect_on_unapproved'] );
+		update_option( 'ntmrr_redirect_on_unapproved_url', $_POST['ntmrr_redirect_on_unapproved_url'] );
+		update_option( 'ntmrr_whitelisted_emails', $_POST['ntmrr_whitelisted_emails'] );
+		update_option( 'ntmrr_blacklisted_emails', $_POST['ntmrr_blacklisted_emails'] );
+        
+		echo '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
+	}
+	?>
+	<div class="wrap">
+		<form name="ntmrr_options_form" method="POST" action="">
+		<input type="hidden" name="ntmrr_posted" value="Y" />
+			<h2>Emails</h2>
+			<div style="width: 49%; float: left;">
+				<h3><label for="ntmrr_whitelisted_emails">Email WhiteList</label></h3>
+				<p>These are the <strong>accepted</strong> emails. Users <em>must</em> use an email represented on this list in order to register. Any emails not on this list will be rejected</p>
+				<ul>
+					<li>One address per line</li>
+					<li>You may use * as a wildcard, but...
+					<ul>
+						<li>Make sure the @ is included in each email address ("*abc.org" would match "bob@badmanabc.org")</li>
+						<li>Try to stick to *@whatever.org or certain_name@gmail.com</li>
+						<li>Don't go crazy with the *'s for security's sake (*_*@*.abc*.org is what NOT to do)</li>
+						<li>*@abc.org is a good usage</li>
+					</ul>
+					</li>
+				</ul>
+				<textarea rows="15" name="ntmrr_whitelisted_emails"><?php echo $whitelist; ?></textarea>
+			</div>
+			<div style="width: 49%; float: right;">
+				<h3><label for="ntmrr_whitelisted_emails">Email BlackList</label></h3>
+				<p>These are <strong>blocked</strong> emails. Users who register with emails in this list wil be denied access, even if their emails are on the whitelist</p>
+				<ul>
+					<li>One address per line</li>
+					<li>You may <strong>NOT</strong> use * as a wildcard</li>
+					<li>Each entry must be a complete, valid email address</li>
+					<li>Remember, this overrides the WhiteList.</li>
+					<li>Useful for blocking specific people in approved organizations (like stephen_the_hacker@abc.org)</li>
+				</ul>
+				<textarea rows="15" name="ntmrr_blacklisted_emails"><?php echo $blacklist; ?></textarea>
+			</div>
+			<p class="submit" style="clear: both;">
+				<input type="submit" name="Submit1" class="button-primary" value="Save Changes" />
+			</p>
+			
+			<h2 style="clear: both;">Options</h2>
+			<label for="ntmrr_registration_form_message">Registration Form Message</label>
+			<p>This message appears above the WP-Members registration form. It is useful for telling them their email address needs to be from certain domains or be "pre-approved".</p>
+			<p>Example: &lt;p&gt;The email address you use must be on the pre-approved list&lt;/p&gt;</p>
+			<input type="text" name="ntmrr_registration_form_message" value="<?php echo $reg_form_msg; ?>" /><br/><br/>
+			
+			<label for="ntmrr_email_not_approved_message">"Email Not Approved" Message</label>
+			<p>This is the error message that appears when attempting to register using an unapproved email address.</p>
+			<p>Example: &lt;p&gt;We're sorry. You are using an E-mail address that has not been pre-approved.&lt;/p&gt;</p>
+			<input type="text" name="ntmrr_email_not_approved_message" value="<?php echo $error_msg; ?>" /><br/><br/>
+			
+			<input type="checkbox" name="ntmrr_redirect_on_unapproved" value="1" <?php echo $redirect ? 'checked' : ''; ?> /><label for="ntmrr_redirect_on_unapproved">Redirect when email is not approved?</label>
+			<p>To redirect on error instead of just showing a message, check the box and fill in the location below</p>
+			<p>Be sure to use valid url (ie 'https://' . $_SERVER['SERVER_NAME'] . '/YOUR-FAILURE-PAGE/')</p>
+			<label for="ntmrr_redirect_on_unapproved_url">URL to redirect to: </label><input type="text" name="ntmrr_redirect_on_unapproved_url" value="<?php echo $url; ?>" />
+			<p>Use [ntmrr_registration_error] on the redirect page to show the error on that page if needed</p><br/>
+			
+			<p class="submit" style="clear: both;">
+				<input type="submit" name="Submit2" class="button-primary" value="Save Changes" />
+			</p>
+		</form>
+	</div>
+	<?php
+}
+function ntmrr_add_options_menu() {
+	add_users_page( 'Restrict Registration By Email', 'Pre-Approve', 'manage_options', 'ntmrr_options_menu', 'ntmrr_options' );
+}
+add_action( 'admin_menu', 'ntmrr_add_options_menu' );
 
 ?>
